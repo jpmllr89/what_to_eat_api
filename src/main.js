21c52b7cb5da9fffede0bd0d96ec7e49acf36aac
Jesse Miller <jpmllr89@gmail.com>
2023-11-21 18:43:12 -0500

    invoke api and generate html to add onto screen.  takes 10 random recipes

--- src/main.js
+++ src/main.js
@@ -1,7 +1,86 @@
-let url = `www.themealdb.com/api/json/v1/1/filter.php?i=${name_here}`
 
+// declare global variables
+const input = document.getElementById('foodName').textContent;
 const foodItem = document.getElementById('foodItem');
+const container = document.getElementById('container');
+console.log(container);
 
-foodItem.addEventListener('click', () => {
+// const htmlGenerators = {
+//   card:{
+//     meal: (data) => `<h1>${data}</h1>`,
+//     thumb: (data) => `<img src="${data}" alt="Meal Thumbnail">`,
+//     instructions: (data) => data.map(instruction => `<p>${instruction}</p>`).join('')
+//   }
+// };
 
+const getIngredients = (data) => {
+  const ingredients = [];
+  const iterateData = data.meals[0];
+  console.log
+  for (let i = 1; i <= 20; i++) {
+    const ingredient = iterateData[`strIngredient${i}`];
+    if (ingredient && ingredient !== '') {
+      ingredients.push(ingredient);
+    }
+  }
+  return ingredients;
+}
+
+const getMeasurements = (data) => {
+  const measurements = [];
+  const iterateData = data.meals[0];
+  for (let i = 1; i <= 20; i++) {
+    const measurement = iterateData[`strMeasure${i}`];
+    if (measurement && measurement !== '') {
+      measurements.push(measurement);
+    }
+  }
+  return measurements;
+}
+
+const generateCard = async (url) => {
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log(data.meals[0]['strIngredient1']);
+
+  const mealHtml = data.meals[0].strMeal;
+  const thumbHtml = data.meals[0].strMealThumb;
+  const instructionsHtml = data.meals[0].strInstructions.replace(/'[0-9].' || 'STEP [0-9]'/g, '').split('\n').map(instruction => `<p>${instruction}</p>`).join('');
+  const ingredients = getIngredients(data);
+  const measurements = getMeasurements(data);
+
+  const ingredientsHtml = ingredients.map((ingredient, i) => {
+    const measurement = measurements[i] || '';
+    return `<p>${measurement} ${ingredient}</p>`;
+  }).join('');
+
+  const finalHtml = `
+  <div class='recipe'>
+    <h1>${mealHtml}</h1> 
+    <img src="${thumbHtml}" alt="Meal Thumbnail">
+    <h2>Ingredients</h2>
+    <div>${ingredientsHtml}</div>
+    <h2>Instructions</h2>
+    <div>${instructionsHtml}</div>
+  </div>
+  `;
+  
+  console.log(finalHtml);
+  return finalHtml;
+
+  // You can then append `finalHtml` to your DOM or use it as needed.
+};
+
+generateCard(`https://www.themealdb.com/api/json/v1/1/random.php`);
+
+
+
+// console.log(input);
+foodItem.addEventListener('click', async () => {
+  for (let i = 0; i < 10; i++) {
+    const html = await generateCard(`https://www.themealdb.com/api/json/v1/1/random.php`);
+    container.innerHTML += html;
+  }
 });
+
+// console.log(url);
