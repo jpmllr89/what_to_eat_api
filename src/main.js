02aae4a4279e9fcc10b46e38a828d24d499fa117
Jesse Miller <jpmllr89@gmail.com>
2023-11-27 20:22:59 -0500

    fix heart icon to add to favorites and add back to container

--- src/main.js
+++ src/main.js
@@ -6,19 +6,9 @@ const container = document.getElementById('container');
 const recipes = Array.from(document.getElementsByClassName('recipe'));
 const addFavoriteIcon = document.getElementById('add');
 let idCounter = 0;
+const favoriteList = document.getElementById('favorites');
 
-console.log(addFavoriteIcon);
-
-// const htmlGenerators = {
-//   card:{
-//     meal: (data) => `<h1>${data}</h1>`,
-//     thumb: (data) => `<img src="${data}" alt="Meal Thumbnail">`,
-//     instructions: (data) => data.map(instruction => `<p>${instruction}</p>`).join('')
-//   }
-// };
-const addFavorite = () => {
-  addFavoriteIcon.style.color = 'red';
-};
+// This function returns an array of ingredients from the data object.
 const getIngredients = (data) => {
   const ingredients = [];
   const iterateData = data.meals[0];
@@ -32,6 +22,7 @@ const getIngredients = (data) => {
   return ingredients;
 }
 
+// This function returns an array of measurements from the data object.
 const getMeasurements = (data) => {
   const measurements = [];
   const iterateData = data.meals[0];
@@ -44,10 +35,9 @@ const getMeasurements = (data) => {
   return measurements;
 }
 
-const generateCard = async (url, i) => {
+const generateRecipe = async (url, i) => {
   const response = await fetch(url);
   const data = await response.json();
-  console.log(data.meals[0]['strIngredient1']);
 
   const mealHtml = data.meals[0].strMeal;
   const thumbHtml = data.meals[0].strMealThumb;
@@ -62,7 +52,7 @@ const generateCard = async (url, i) => {
 
   const finalHtml = `
   <div class='recipe' id="${idCounter}">
-    <div class="row justify-content"><h2>${mealHtml}</h2><i id="add" onClick="addFavorite()">&hearts; Add</i></div>
+    <div class="row justify-content"><h2>${mealHtml}</h2><i class="fa-solid fa-heart"></i></div>
     <img src="${thumbHtml}" alt="Meal Thumbnail">
     <h2>Ingredients</h2>
     <div>${ingredientsHtml}</div>
@@ -78,23 +68,58 @@ const generateCard = async (url, i) => {
   // You can then append `finalHtml` to your DOM or use it as needed.
 };
 
-generateCard(`https://www.themealdb.com/api/json/v1/1/random.php`);
+generateRecipe(`https://www.themealdb.com/api/json/v1/1/random.php`);
 
 
 
 // console.log(input);
 foodItem.addEventListener('click', async () => {
   for (let i = 0; i < 10; i++) {
-    const html = await generateCard(`https://www.themealdb.com/api/json/v1/1/random.php`, i);
+    const html = await generateRecipe(`https://www.themealdb.com/api/json/v1/1/random.php`, i);
     container.innerHTML += html;
   }
+  const recipes = Array.from(document.getElementsByClassName('recipe'));
+  recipes.forEach((item)=>{
+    item.addEventListener('click', function(e){
+      const recipe = e.target.parentNode.parentNode.id;
+      const target = e.target.parentNode.parentNode.parentNode.id;
+      console.log(target, "target");
+      console.log(recipe, "recipe");
+      // console.log(recipe.id, "recipe");
+      target == 'container' ? moveCard(recipe, 'toFavs') : moveCard(recipe, 'toMain')
+  })});
 });
 
 
-container.addEventListener('click', (event) => {
-  if (event.target && event.target.id === 'add') {
-    const recipeCard = event.target.parentElement.parentElement;
+const moveCard = (id, direction) =>{
+
+  const selection = document.getElementById(id);
+  console.log(selection);
+
+  if(selection){
+    selection
+      .querySelector('i')
+      .classList.toggle('fa-heart-broken', direction ==="toFavs");
     
+    selection
+      .querySelector('i')
+      .classList.toggle('fa-heart', direction === 'toMain');
+    
+    
+    const targetParent = 
+      direction === 'toMain' 
+        ? document.getElementById('container') 
+        : document.getElementById('favorites');
+    
+    targetParent.appendChild(selection);
+
   }
-});
-// console.log(url);
+}
+
+
+// recipes.forEach((item)=>{
+//   item.addEventListener('click', function(e){
+//     const target = e.target.parentNode.id;
+//     console.log(target);
+//     target == 'main' ? updateCollections(item.id, 'toFavs') : updateCollections(item.id, 'toMain')
+// })});
