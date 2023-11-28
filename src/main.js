a77ea6fbb7db51ef55d1f750e549d284b53c3b9a
Jesse Miller <jpmllr89@gmail.com>
2023-11-28 16:21:37 -0500

    new commits because I just deleted a branch without knowing that it would delete a chunk of my js.  Gotta push

--- src/main.js
+++ src/main.js
@@ -4,11 +4,15 @@
 const foodItem = document.getElementById('foodItem');
 const container = document.getElementById('container');
 const recipes = Array.from(document.getElementsByClassName('recipe'));
-const switchButton = document.querySelectorAll('div.recipes i');
-let idCounter = 0;
-const favoriteList = document.getElementById('favorites');
+const addFavoriteIcon = document.getElementById('add');
+const addButtonSelector ='div.recipes i';
 
-// This function returns an array of ingredients from the data object.
+console.log(recipes);
+
+
+const addFavorite = () => {
+  addFavoriteIcon.style.color = 'red';
+};
 const getIngredients = (data) => {
   const ingredients = [];
   const iterateData = data.meals[0];
@@ -22,7 +26,6 @@ const getIngredients = (data) => {
   return ingredients;
 }
 
-// This function returns an array of measurements from the data object.
 const getMeasurements = (data) => {
   const measurements = [];
   const iterateData = data.meals[0];
@@ -35,9 +38,10 @@ const getMeasurements = (data) => {
   return measurements;
 }
 
-const generateRecipe = async (url, i) => {
+const generateCard = async (url) => {
   const response = await fetch(url);
   const data = await response.json();
+  console.log(data.meals[0]['strIngredient1']);
 
   const mealHtml = data.meals[0].strMeal;
   const thumbHtml = data.meals[0].strMealThumb;
@@ -51,8 +55,8 @@ const generateRecipe = async (url, i) => {
   }).join('');
 
   const finalHtml = `
-  <div class='recipe' id="${idCounter}">
-    <div class="row justify-content"><h2>${mealHtml}</h2><i id="add" class="fa-solid fa-heart"></i></div>
+  <div class='recipe'>
+    <div class="row justify-content"><h2>${mealHtml}</h2><i id="add" onClick="addFavorite()">&hearts; Add</i></div>
     <img src="${thumbHtml}" alt="Meal Thumbnail">
     <h2>Ingredients</h2>
     <div>${ingredientsHtml}</div>
@@ -61,65 +65,34 @@ const generateRecipe = async (url, i) => {
   </div>
   `;
   
-  idCounter++;
   console.log(finalHtml);
   return finalHtml;
 
   // You can then append `finalHtml` to your DOM or use it as needed.
 };
 
-generateRecipe(`https://www.themealdb.com/api/json/v1/1/random.php`);
+generateCard(`https://www.themealdb.com/api/json/v1/1/random.php`);
 
 
 
 // console.log(input);
 foodItem.addEventListener('click', async () => {
   for (let i = 0; i < 10; i++) {
-    const html = await generateRecipe(`https://www.themealdb.com/api/json/v1/1/random.php`, i);
+    const html = await generateCard(`https://www.themealdb.com/api/json/v1/1/random.php`);
     container.innerHTML += html;
   }
-  const recipes = Array.from(document.querySelectorAll(switchButton));
-  recipes.forEach((item)=>{
-    item.addEventListener('click', function(e){
-      const recipe = e.target.parentNode.parentNode.id;
-      const target = e.target.parentNode.parentNode.parentNode.id;
-      console.log(target, "target");
-      console.log(recipe, "recipe");
-      // console.log(recipe.id, "recipe");
-      target == 'container' ? moveCard(recipe, 'toFavs') : moveCard(recipe, 'toMain')
-  })});
 });
 
+addFavorite.addEventListener('click', () => {
+  addFavorite.style.color = 'red';
+});
 
-const moveCard = (id, direction) =>{
+// right here is the switching functionality
+Array.from(allItems).forEach((item)=>{
+  item.addEventListener('click', function(e){
+    const target = e.target.parentNode.id;
+    console.log(target);
+    target == 'main' ? updateCollections(item.id, 'toFavs') : updateCollections(item.id, 'toMain')
+})});
 
-  const selection = document.getElementById(id);
-  console.log(selection);
-
-  if(selection){
-    selection
-      .querySelector('i')
-      .classList.toggle('fa-heart-broken', direction ==="toFavs");
-    
-    selection
-      .querySelector('i')
-      .classList.toggle('fa-heart', direction === 'toMain');
-    
-    
-    const targetParent = 
-      direction === 'toMain' 
-        ? document.getElementById('container') 
-        : document.getElementById('favorites');
-    
-    targetParent.appendChild(selection);
-
-  }
-}
-
-
-// recipes.forEach((item)=>{
-//   item.addEventListener('click', function(e){
-//     const target = e.target.parentNode.id;
-//     console.log(target);
-//     target == 'main' ? updateCollections(item.id, 'toFavs') : updateCollections(item.id, 'toMain')
-// })});
+// console.log(url);
